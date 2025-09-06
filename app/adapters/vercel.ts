5d97740ac19e89f1f4ff072067d6793bd00fd40c
Rainier-Developer <services@webcommerce.cloud>
2025-09-06 22:40:47 +0200

    Remove Vercel config files causing deployment errors

--- app/adapters/vercel.ts
+++
@@ -1,6 +0,0 @@
-import type { ServerBuild } from "@react-router/node";
-import { createRequestHandler } from "@react-router/node";
-
-export default function createVercelRequestHandler(build: ServerBuild) {
-  return createRequestHandler(build, "production");
-}
